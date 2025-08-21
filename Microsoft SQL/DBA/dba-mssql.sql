e0ff9414961eaba3d713f9707746d508913556f7
Kirill Rumiantsev <kyrylo.rumiantsev@nixs.com>
2025-08-21 14:42:07 +0300

    columns with PK

--- Microsoft SQL/DBA/dba-mssql.sql
+++ Microsoft SQL/DBA/dba-mssql.sql
@@ -24,3 +24,20 @@ select @sql;
 
 EXEC sp_executesql @sql;
 
+-- columns with PK:
+SELECT
+  s.name  AS schema_name,
+  t.name  AS table_name,
+  c.name  AS column_name,
+  ic.key_ordinal,
+  kc.type
+FROM sys.key_constraints kc
+JOIN sys.indexes        i  ON kc.parent_object_id = i.object_id AND kc.unique_index_id = i.index_id
+JOIN sys.index_columns  ic ON i.object_id = ic.object_id AND i.index_id = ic.index_id
+JOIN sys.columns        c  ON ic.object_id = c.object_id AND ic.column_id = c.column_id
+JOIN sys.tables         t  ON t.object_id = kc.parent_object_id
+JOIN sys.schemas        s  ON s.schema_id = t.schema_id
+WHERE kc.type = 'PK'
+ORDER BY s.name, t.name, ic.key_ordinal;
+
+
